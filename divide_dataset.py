21201a84674ba122cc0d5c8389aa0844f6487385
user media <kanlight28@gmail.com>
2025-01-18 22:38:40 +0900

    コメントや変数名の整理

--- divide_dataset.py
+++ divide_dataset.py
@@ -2,10 +2,11 @@ import random
 import json
 import os
 
-batch_size = 8
+pack_size = 8
 dataset_dir = 'DatasetByLuke'
 
-def generate_batch_user(filename):
+def make_package(filename):
+    # パックサイズに従って対話データを分割
     # [a,b,c,d,e,f] -> [[a,b],[c,d],[e,f]] (ex.batch size 2)
     talk_data = []
     data = {}
@@ -18,29 +19,29 @@ def generate_batch_user(filename):
     
     # 対話をシャッフル
     random.shuffle(talk_data)
-    # バッチサイズに従って分割
-    batch_list = []
-    for i in range(len(talk_data) // batch_size):
-        batch_list.append(talk_data[i:i+batch_size])
+    # パックサイズに従って分割
+    packs = []
+    for i in range(len(talk_data) // pack_size):
+        packs.append(talk_data[i:i+pack_size])
 
-    return batch_list
+    return packs
 
 def main():
-    # 各ユーザーのファイルからミニバッチ(のようなリスト)を作成
-    batches = []
+    # 各ユーザーのファイルからパックを作成
+    packs = []
     for filename in os.listdir(dataset_dir):
-        batch_list = generate_batch_user(filename)
-        batches = batches + batch_list
+        pack = make_package(filename)
+        packs = packs + pack
     
-    # ミニバッチをシャッフル
-    random.shuffle(batches)
+    # パックをシャッフル
+    random.shuffle(packs)
     # 6:2:2で学習:検証:テストデータに分割
-    n = len(batches)
+    n = len(packs)
     n_train = int(0.6*n)
     n_val = int(0.2*n)
-    dataset_train = {"data": batches[:n_train]}
-    dataset_val = {"data": batches[n_train:n_train+n_val]}
-    dataset_test = {"data": batches[n_train+n_val:]}
+    dataset_train = {"data": packs[:n_train]}
+    dataset_val = {"data": packs[n_train:n_train+n_val]}
+    dataset_test = {"data": packs[n_train+n_val:]}
     # 保存
     with open('DatasetTrain.json', 'w', encoding='utf-8') as f:
         json.dump(dataset_train, f, indent=4, ensure_ascii=False)
